1c31c4fb029683d4eeb4bf848a20ca1448c44fa4
Trent Walraven <trwbox@gmail.com>
2022-09-19 00:19:20 -0500

    Work after Sunday done to 2e. Need to create test

--- print.py
+++ print.py
@@ -4,11 +4,35 @@ for i in range(2):
 		for k in range(2):
 			for l in range(2):
 				for m in range(2):
-					# This prints the testbench for the 32 bit 32 to 1 mux
-					print(Test case and number and excpected output)
-					print(Loading the inputs with stair stepping bits)
-					print(Going through all the values of the i_SEL to showit runs through them)
+					# Print the values for the register file
+					print(f'                data_input{count} => registerVector({count}),')
 					
+					# For whole register file
+					#print(f'    signal data_{count} : std_logic_vector(31 downto 0) := "00000000000000000000000000000000";')
+
+					# This prints the testbench for the 32 bit 32 to 1 mux
+					# print(f'        -- Test Case {count+1+32}')
+					# print(f'        -- This is testing "{i}{j}{k}{l}{m}" and EN = \'0\' should be an output of "0000000000000000"')
+					# print(f'        s_IN <= "{i}{j}{k}{l}{m}";')
+					# print("        wait for gCLK_HPER * 2;")
+					# print()
+
+					# This prints the testbench for the 32 bit 32 to 1 mux
+					# print(f'        -- Test Case {count+1}')
+					# print(f'        -- This is testing "{i}{j}{k}{l}{m}" should be an output of "00000000000000000000000000000000"')
+					# print(f'        s_IN <= "{i}{j}{k}{l}{m}";')
+					# print("        wait for gCLK_HPER * 2;")
+					# print()
+					
+					# Print out the data input signal
+					#print(f"   signal s_data_input{count} : std_logic_vector(31 downto 0) := \"00000000000000000000000000000000\";")
+
+					# Print out the setting of data
+					#print(f'        s_data_input{count} <= "00000000000000000000000000000000";')
+
+					# Print out the data input
+					#print(f"        data_input{count} => s_data_input{count},")
+
 					# This prints out the process for dataflow VHDL for 32 bit 32 to 1 mux
 					#print(f'        when "{i}{j}{k}{l}{m}" => data_output <= data_input{count};')
 					
